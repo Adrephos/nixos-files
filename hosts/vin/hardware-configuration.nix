6a4c276d86f262ca77fd026f96f16622a9ab9891
Adrephos <adrephos@gmail.com>
2024-03-27 16:36:45 -0500

    feat: add swapfile config

--- hosts/vin/hardware-configuration.nix
+++ hosts/vin/hardware-configuration.nix
@@ -25,7 +25,10 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [ {
+    device = "/var/lib/swapfile";
+    size = 16*1024;
+  } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -36,5 +39,4 @@
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
