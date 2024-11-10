5cb3fdadd3e77f3285647d000dd31c5fc6e427d2
Adrephos <adrephos@gmail.com>
2024-11-09 21:31:05 -0500

    feat: add pipewire fix for pipewire audio stuttering

--- hosts/vin/common/generic/pipewire.nix
+++ hosts/vin/common/generic/pipewire.nix
@@ -11,5 +11,12 @@
       pulse.enable = true;
       jack.enable = true;
     };
+    pipewire.extraConfig.pipewire."92-high-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 2048;
+        "default.clock.min-quantum" = 1024;
+    };
+  };
   };
 }
