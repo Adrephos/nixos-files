7676fd977c8451416d99470165256f2cb0c4d0a5
Juan A. <adrephos@gmail.com>
2025-10-20 15:47:10 -0500

    feat: add ghostty again

--- home/gleipnir/components/cli/ghostty/default.nix
+++ home/gleipnir/components/cli/ghostty/default.nix
@@ -4,20 +4,21 @@
     package = pkgs.ghostty;
     enableFishIntegration = true;
     settings = {
+      keybind = [ "ctrl+shift+j=unbind" ];
       command = "fish";
       working-directory = "home";
       window-inherit-working-directory = false;
       gtk-titlebar = false;
       font-family = "JetBrainsMono NF";
-      font-size = 15;
-      theme = "catppuccin-mocha";
+      font-size = 13;
+      theme = "Catppuccin Mocha";
       background-opacity = 0.9;
       background = "#101119";
       cursor-text = "#000000";
       window-padding-x = 10;
       window-padding-y = 10;
       gtk-wide-tabs = false;
-      gtk-adwaita = true;
+      # gtk-adwaita = true;
       gtk-single-instance = true;
     };
   };
