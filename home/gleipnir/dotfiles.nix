6358195dede311b98472e69d801707924898ce64
Juan A. <adrephos@gmail.com>
2026-08-18 16:40:13 -0500

    feat: declare rofi config in nix

--- home/gleipnir/dotfiles.nix
+++ home/gleipnir/dotfiles.nix
@@ -3,5 +3,8 @@
     "herdr/config.toml".source = ../../dotfiles/herdr/config.toml;
     "niri/config.kdl".source = ../../dotfiles/niri/config.kdl;
     "fcitx5/profile".source = ../../dotfiles/fcitx5/profile;
+    "rofi/config.rasi".source = ../../dotfiles/rofi/config.rasi;
+    "rofi/catppuccin-default.rasi".source = ../../dotfiles/rofi/catppuccin-default.rasi;
+    "rofi/catppuccin-mocha.rasi".source = ../../dotfiles/rofi/catppuccin-mocha.rasi;
   };
 }
