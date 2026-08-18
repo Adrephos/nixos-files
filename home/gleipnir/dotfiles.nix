a3cbeacaac8a3f197a30dc7b7d7301c3ff2f417b
Juan A. <adrephos@gmail.com>
2026-08-18 15:55:21 -0500

    feat: add fcitx5 hangul input and declarative profile

--- home/gleipnir/dotfiles.nix
+++ home/gleipnir/dotfiles.nix
@@ -2,5 +2,6 @@
   xdg.configFile = {
     "herdr/config.toml".source = ../../dotfiles/herdr/config.toml;
     "niri/config.kdl".source = ../../dotfiles/niri/config.kdl;
+    "fcitx5/profile".source = ../../dotfiles/fcitx5/profile;
   };
 }
--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -9,6 +9,7 @@
       enable = true;
       fcitx5.addons = with pkgs; [
         fcitx5-mozc
+        fcitx5-hangul
         fcitx5-gtk
         libsForQt5.fcitx5-qt
         kdePackages.fcitx5-qt
