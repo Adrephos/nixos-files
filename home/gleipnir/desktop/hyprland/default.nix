aad74720c022e8f8cd123daff5f779ce8d863e3b
Juan A. <adrephos@gmail.com>
2026-08-19 15:53:32 -0500

    fix: stop leaking hyprland nvidia env vars into other sessions

--- home/gleipnir/desktop/hyprland/default.nix
+++ home/gleipnir/desktop/hyprland/default.nix
@@ -21,10 +21,7 @@
       rofi
     ];
     sessionVariables = {
-      GBM_BACKEND = "nvidia-drm";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
       ENABLE_VKBASALT = "1";
-      LIBVA_DRIVER_NAME = "nvidia";
       WLR_NO_HARDWARE_CURSORS = "1";
       HYPRSHOT_DIR = "/home/${config.home.username}/Pictures/Screenshots";
     };
