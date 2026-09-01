4c539b9fb2cc51d4593a3f7b4f39cd4915d0a280
Juan A. <adrephos@gmail.com>
2026-08-31 19:34:23 -0500

    feat: mount IDK drive at /srv/media on elend

--- hosts/elend/default.nix
+++ hosts/elend/default.nix
@@ -5,5 +5,13 @@
   ];
 
   networking.hostName = "elend";
+
+  # Existing general-purpose HDD, labeled "IDK" (Pictures, github.com backups, Video/Anime, Video/College).
+  fileSystems."/srv/media" = {
+    device = "/dev/disk/by-label/IDK";
+    fsType = "ext4";
+    options = [ "nofail" ];
+  };
+
   system.stateVersion = "23.11";
 }
