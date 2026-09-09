6f3aece27bcba83d3d19575a76864d81a3ace700
Juan A. <adrephos@gmail.com>
2026-09-09 11:38:23 -0500

    feat: set helium as default browser

--- home/gleipnir/desktop/default-apps.nix
+++ home/gleipnir/desktop/default-apps.nix
@@ -1,6 +1,7 @@
 { lib, ... }:
 let
-  defaultBrowser = "brave-browser-nightly.desktop";
+  # defaultBrowser = "brave-browser-nightly.desktop";
+  defaultBrowser = "helium.desktop";
   defaultImageViewer = "org.gnome.Loupe.desktop";
   defaultEditor = "nvim-kitty.desktop";
   defaultFileManager = "org.gnome.Nautilus.desktop";
